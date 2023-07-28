fe31221ce55bd21463bd447770acd1ea3cf6a096
Jarvis <13g7895123@gmail.com>
2023-07-28 20:01:01 +0800

    [edit] revise route

--- vue-app/src/router/routes.js
+++ vue-app/src/router/routes.js
@@ -5,31 +5,16 @@ const routes = [
         component: () => import('../views/Home.vue'),
         children: [
             { 
-                path: "/money",
+                path: "/checkAccount",
                 component: () => import('../views/Money.vue'),
                 // children :[
                 //     { path: "/bonus", component: () => import('../views/Bonus.vue')},
                 // ]
             },
-            { path: "/bonus", component: () => import('../views/Bonus.vue')},
+            { path: "/forgetPassword", component: () => import('../views/Bonus.vue')},
             { path: "/test", component: () => import('../views/Test.vue')},
         ]
     },
-    // {
-    //     path: "/register",
-    //     name: 'Register',
-    //     component: () => import('../views/Register.vue'),
-    // },
-    // {
-    //     path: "/login",
-    //     name: 'Login',
-    //     component: () => import('../views/Login.vue'),
-    // },
-    // {
-    //     path: '/:catchAll(.*)',
-    //     name: '404',
-    //     component: () => import('../views/404.vue')
-    // }
 ]
 
 export default routes
