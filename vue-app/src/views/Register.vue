8961eaccd98422c33c98c9784cf796dfb1a88657
Jarvis <13g7895123@gmail.com>
2023-08-08 20:57:37 +0800

    [add] page register

--- vue-app/src/views/Register.vue
+++ vue-app/src/views/Register.vue
@@ -1,3 +1,18 @@
 <template>
-    <div class="text-red">test</div>
+    <!-- login box -->
+    <div>
+        <form @submit.prevent="submit">
+            <h2>Login</h2>
+            <!-- input box -->
+            <div>
+                <!-- icon -->
+                <span></span>
+            </div>
+        </form>
+    </div>
 </template>
+<script setup>
+const submit = () => {
+
+}
+</script>
