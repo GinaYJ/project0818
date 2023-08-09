79df998d29d2f5252d82c22425a1f99b68c734e2
Jarvis <13g7895123@gmail.com>
2023-08-09 21:17:27 +0800

    [edit] revise render

--- vue-app/src/views/Register.vue
+++ vue-app/src/views/Register.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- login box -->
-    <div class="w-screen h-screen">
+    <div class="w-screen h-screen flex justify-center">
         <form @submit.prevent="submit" class="px-3 items-center justify-center" style="width: 300px; display: flex; flex-direction: column;">
             <h2>Login</h2>
             <!-- input box -->
