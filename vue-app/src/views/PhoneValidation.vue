6df5da999859131d158f179f5a97f078ac5fcd41
Jarvis <13g7895123@gmail.com>
2023-08-12 18:49:31 +0800

    [edit] revise render

--- vue-app/src/views/PhoneValidation.vue
+++ vue-app/src/views/PhoneValidation.vue
@@ -14,7 +14,7 @@
                 <div class="inp_group mt-2">
                     <input required>
                     <span>認證碼</span>
-                    <i style="width: 48%;"></i>
+                    <i style="width: 60%;"></i>
                     <div id='btn_send_code' class="btn" style="cursor: pointer;">發送認證碼</div>
                 </div>
                 <div class="bg-white rounded flex justify-center items-center btn py-1 mb-2 mt-6">送出</div>
