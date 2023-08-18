5a15a6a70a669883893709447a8910e5ca1c2207
Jarvis <13g7895123@gmail.com>
2023-08-18 20:47:36 +0800

    [edit] fix error

--- vue-app/src/views/PhoneValidation.vue
+++ vue-app/src/views/PhoneValidation.vue
@@ -29,11 +29,11 @@
 import axios from 'axios'
 import Swal from 'sweetalert2'
 
-// const phone = ref()
-// const code = ref()
-// const apiUrl = ref()
-// const apiParam = ref()
-// const apiUrlPrefix = 'http://170.187.229.132:9090/api/bonus-register/'
+const phone = ref()
+const code = ref()
+const apiUrl = ref()
+const apiParam = ref()
+const apiUrlPrefix = 'http://170.187.229.132:9090/api/bonus-register/'
 
 // const sendCode = async() => {
 //     if (phone.value != ''){     // 驗證手機號碼不可為空值
