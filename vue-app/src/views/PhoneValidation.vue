495735605ad86fc98404782a6fb7a67e96b603f8
Jarvis <13g7895123@gmail.com>
2023-08-18 20:47:52 +0800

    [edit] fix error

--- vue-app/src/views/PhoneValidation.vue
+++ vue-app/src/views/PhoneValidation.vue
@@ -33,7 +33,7 @@ const phone = ref()
 const code = ref()
 const apiUrl = ref()
 const apiParam = ref()
-const apiUrlPrefix = 'http://170.187.229.132:9090/api/bonus-register/'
+// const apiUrlPrefix = 'http://170.187.229.132:9090/api/bonus-register/'
 
 // const sendCode = async() => {
 //     if (phone.value != ''){     // 驗證手機號碼不可為空值
